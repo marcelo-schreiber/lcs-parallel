54013230c3eecd91aff6ffe3fd3678873d0e7b81
marcelo_augusto <marcelo.rissette@vidyatec.com>
2025-06-20 22:43:58 -0300

    refactor LCS function to improve memory management and print time since perf is weird

--- lcs-mpi.c
+++ lcs-mpi.c
@@ -16,7 +16,8 @@ typedef unsigned short mtype;
 char *read_seq(char *fname)
 {
     FILE *fseq = fopen(fname, "rt");
-    if (!fseq) {
+    if (!fseq)
+    {
         printf("Error reading file %s\n", fname);
         exit(1);
     }
@@ -26,13 +27,15 @@ char *read_seq(char *fname)
     rewind(fseq);
 
     char *seq = (char *)calloc(size + 1, sizeof(char));
-    if (!seq) {
+    if (!seq)
+    {
         printf("Error allocating memory for sequence %s.\n", fname);
         exit(1);
     }
 
     int i = 0, ch;
-    while ((ch = fgetc(fseq)) != EOF) {
+    while ((ch = fgetc(fseq)) != EOF)
+    {
         if (ch != '\n')
             seq[i++] = ch;
     }
@@ -42,32 +45,39 @@ char *read_seq(char *fname)
     return seq;
 }
 
-mtype **allocateScoreMatrix(int sizeA, int sizeB)
+mtype **allocateLocalBlock(int blockSize)
 {
-    mtype **scoreMatrix = (mtype **)malloc((sizeB + 1) * sizeof(mtype *));
-    for (int i = 0; i <= sizeB; i++)
-        scoreMatrix[i] = (mtype *)calloc(sizeA + 1, sizeof(mtype));
-    return scoreMatrix;
+    mtype **block = (mtype **)malloc((blockSize + 1) * sizeof(mtype *));
+    for (int i = 0; i <= blockSize; i++)
+        block[i] = (mtype *)calloc(blockSize + 1, sizeof(mtype));
+    return block;
 }
 
-void initScoreMatrix(mtype **scoreMatrix, int sizeA, int sizeB)
+void freeLocalBlock(mtype **block, int blockSize)
 {
-    for (int j = 0; j <= sizeA; j++)
-        scoreMatrix[0][j] = 0;
-    for (int i = 1; i <= sizeB; i++)
-        scoreMatrix[i][0] = 0;
+    for (int i = 0; i <= blockSize; i++)
+        free(block[i]);
+    free(block);
 }
 
-int LCS(mtype **scoreMatrix, int sizeA, int sizeB, char *seqA, char *seqB, int blockSize, int rank, int size)
+int LCS(int sizeA, int sizeB, char *seqA, char *seqB, int blockSize, int rank, int size)
 {
     int blocks_i = (sizeB + blockSize - 1) / blockSize;
     int blocks_j = (sizeA + blockSize - 1) / blockSize;
 
-    mtype *col_buffer = (mtype *)malloc(blockSize * sizeof(mtype));
+    // Local block for computation
+    mtype **localBlock = allocateLocalBlock(blockSize);
+    
+    // Communication buffers
+    mtype *row_buffer = (mtype *)malloc((blockSize + 1) * sizeof(mtype));
+    mtype *col_buffer = (mtype *)malloc((blockSize + 1) * sizeof(mtype));
 
-    for (int k = 0; k <= blocks_i + blocks_j - 2; ++k) {
-        MPI_Barrier(MPI_COMM_WORLD);
-        for (int bi = 0; bi <= k; ++bi) {
+    int final_result = 0;
+
+    for (int k = 0; k <= blocks_i + blocks_j - 2; ++k)
+    {
+        for (int bi = 0; bi <= k; ++bi)
+        {
             int bj = k - bi;
             if (bi >= blocks_i || bj >= blocks_j)
                 continue;
@@ -75,75 +85,87 @@ int LCS(mtype **scoreMatrix, int sizeA, int sizeB, char *seqA, char *seqB, int b
             if ((bi + bj) % size != rank)
                 continue;
 
-            int i_start = bi * blockSize + 1;
-            int j_start = bj * blockSize + 1;
+            int i_start = bi * blockSize;
+            int j_start = bj * blockSize;
             int i_end = min((bi + 1) * blockSize, sizeB);
             int j_end = min((bj + 1) * blockSize, sizeA);
+            
+            int block_height = i_end - i_start;
+            int block_width = j_end - j_start;
 
-            if (bi > 0) {
+            // Initialize boundaries
+            for (int i = 0; i <= block_height; i++)
+                localBlock[i][0] = 0;
+            for (int j = 0; j <= block_width; j++)
+                localBlock[0][j] = 0;
+
+            // Receive top boundary (from above block)
+            if (bi > 0)
+            {
                 int src = ((bi - 1) + bj) % size;
-                MPI_Recv(&scoreMatrix[i_start - 1][j_start], j_end - j_start + 1, MPI_UNSIGNED_SHORT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(row_buffer, block_width + 1, MPI_UNSIGNED_SHORT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                for (int j = 0; j <= block_width; j++)
+                    localBlock[0][j] = row_buffer[j];
             }
-            if (bj > 0) {
+            
+            // Receive left boundary (from left block)
+            if (bj > 0)
+            {
                 int src = (bi + (bj - 1)) % size;
-                int count = i_end - i_start + 1;
-                MPI_Recv(col_buffer, count, MPI_UNSIGNED_SHORT, src, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                for (int i = 0; i < count; ++i)
-                    scoreMatrix[i_start + i][j_start - 1] = col_buffer[i];
-            }
-            if (bi > 0 && bj > 0) {
-                int src = ((bi - 1) + (bj - 1)) % size;
-                MPI_Recv(&scoreMatrix[i_start - 1][j_start - 1], 1, MPI_UNSIGNED_SHORT, src, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(col_buffer, block_height + 1, MPI_UNSIGNED_SHORT, src, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                for (int i = 0; i <= block_height; i++)
+                    localBlock[i][0] = col_buffer[i];
             }
 
-            for (int i = i_start; i <= i_end; ++i) {
-                for (int j = j_start; j <= j_end; ++j) {
-                    if (seqA[j - 1] == seqB[i - 1])
-                        scoreMatrix[i][j] = scoreMatrix[i - 1][j - 1] + 1;
+            // Compute the local block
+            for (int i = 1; i <= block_height; i++)
+            {
+                for (int j = 1; j <= block_width; j++)
+                {
+                    int global_i = i_start + i - 1;
+                    int global_j = j_start + j - 1;
+                    
+                    if (seqA[global_j] == seqB[global_i])
+                        localBlock[i][j] = localBlock[i - 1][j - 1] + 1;
                     else
-                        scoreMatrix[i][j] = max(scoreMatrix[i - 1][j], scoreMatrix[i][j - 1]);
+                        localBlock[i][j] = max(localBlock[i - 1][j], localBlock[i][j - 1]);
                 }
             }
 
-            if (bi < blocks_i - 1) {
+            // Send bottom boundary (to below block)
+            if (bi < blocks_i - 1)
+            {
                 int dest = ((bi + 1) + bj) % size;
-                MPI_Send(&scoreMatrix[i_end][j_start], j_end - j_start + 1, MPI_UNSIGNED_SHORT, dest, 0, MPI_COMM_WORLD);
+                for (int j = 0; j <= block_width; j++)
+                    row_buffer[j] = localBlock[block_height][j];
+                MPI_Send(row_buffer, block_width + 1, MPI_UNSIGNED_SHORT, dest, 0, MPI_COMM_WORLD);
             }
-            if (bj < blocks_j - 1) {
+            
+            // Send right boundary (to right block)
+            if (bj < blocks_j - 1)
+            {
                 int dest = (bi + (bj + 1)) % size;
-                int count = i_end - i_start + 1;
-                for (int i = 0; i < count; ++i)
-                    col_buffer[i] = scoreMatrix[i_start + i][j_end];
-                MPI_Send(col_buffer, count, MPI_UNSIGNED_SHORT, dest, 1, MPI_COMM_WORLD);
+                for (int i = 0; i <= block_height; i++)
+                    col_buffer[i] = localBlock[i][block_width];
+                MPI_Send(col_buffer, block_height + 1, MPI_UNSIGNED_SHORT, dest, 1, MPI_COMM_WORLD);
             }
-            if (bi < blocks_i - 1 && bj < blocks_j - 1) {
-                int dest = ((bi + 1) + (bj + 1)) % size;
-                MPI_Send(&scoreMatrix[i_end][j_end], 1, MPI_UNSIGNED_SHORT, dest, 2, MPI_COMM_WORLD);
+
+            // Store result if this is the last block
+            if (bi == blocks_i - 1 && bj == blocks_j - 1)
+            {
+                final_result = localBlock[block_height][block_width];
             }
         }
     }
 
+    freeLocalBlock(localBlock, blockSize);
+    free(row_buffer);
     free(col_buffer);
 
-    int result = 0;
-    int last_block_i = (sizeB - 1) / blockSize;
-    int last_block_j = (sizeA - 1) / blockSize;
+    int global_result;
+    MPI_Reduce(&final_result, &global_result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    if (((last_block_i + last_block_j) % size) == rank) {
-        result = scoreMatrix[sizeB][sizeA];
-    }
-
-    int final_result;
-    MPI_Reduce(&result, &final_result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-
-    return final_result;
-}
-
-void freeScoreMatrix(mtype **scoreMatrix, int sizeB)
-{
-    for (int i = 0; i <= sizeB; i++)
-        free(scoreMatrix[i]);
-    free(scoreMatrix);
+    return global_result;
 }
 
 int main(int argc, char **argv)
@@ -154,8 +176,10 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (argc < 2) {
-        if (rank == 0) printf("Usage: ./mpi <blockSize>\n");
+    if (argc < 2)
+    {
+        if (rank == 0)
+            printf("Usage: ./mpi <blockSize>\n");
         MPI_Finalize();
         return 1;
     }
@@ -168,15 +192,19 @@ int main(int argc, char **argv)
     int sizeA = strlen(seqA);
     int sizeB = strlen(seqB);
 
-    mtype **scoreMatrix = allocateScoreMatrix(sizeA, sizeB);
-    initScoreMatrix(scoreMatrix, sizeA, sizeB);
+    double start_time = MPI_Wtime();
 
-    int result = LCS(scoreMatrix, sizeA, sizeB, seqA, seqB, blockSize, rank, size);
+    int result = LCS(sizeA, sizeB, seqA, seqB, blockSize, rank, size);
+
+    double end_time = MPI_Wtime();
+    double elapsed = end_time - start_time;
 
     if (rank == 0)
+    {
         printf("LCS length: %d\n", result);
+        printf("Time elapsed: %.6f seconds\n", elapsed);
+    }
 
-    freeScoreMatrix(scoreMatrix, sizeB);
     free(seqA);
     free(seqB);
 
